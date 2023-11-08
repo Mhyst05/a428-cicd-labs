c2203b02cc3811fc6f8267f01e13562f0289e5fc
Mhyst05 <agungboy615@gmail.com>
2023-11-08 15:16:09 +0700

    Mengubah pesan 'Welcome to React!' menjadi 'Proyek Akhir Membangun CI/CD Pipeline dengan Jenkins Prometheus Grafana'

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Proyek Akhir Membangun CI/CD Pipeline dengan Jenkins Prometheus Grafana</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Credit : Agung Romadhon
         </p>
       </div>
     );
